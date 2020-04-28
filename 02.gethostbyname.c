a3a4c573091724bfbd4b87d454501d98564d0f2f
TDarkT <minhlong1312@gmail.com>
2020-04-27 21:23:55 -0700

    Update Labwork2

--- 02.gethostbyname.c
+++ 02.gethostbyname.c
@@ -3,11 +3,21 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <arpa/inet.h>
-
+#include <stdlib.h>
 int main(int argc, char** argv)
 {
+   if (argc == 1)
+   {
+   	printf("Usage: %s <url>\n", argv[0]);
+	exit(1);
+   }
    struct hostent* ip = gethostbyname(argv[1]);
+   if (ip == NULL)
+   {
+   	printf("Error resolve url\n");
+        exit(1);	
+   }
    struct in_addr ip_addr = *(struct in_addr*)(ip->h_addr);
-   printf("%s",inet_ntoa(ip_addr));   
+   printf("%s: %s\n",argv[1],inet_ntoa(ip_addr));   
    return 0; 
 }
