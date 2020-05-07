bad158eb19660021850ddc5a288b833edf4c1999
TDarkT <minhlong1312@gmail.com>
2020-05-06 20:06:43 -0700

    Add labwork 4

---
+++ 04.pratical.work.client.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <string.h>
+int main(int argc, char **argv) {
+	struct sockaddr_in ad;
+	struct hostent *host;
+	
+	if (argc == 1) {
+		printf("Usage %s <ip>\n",argv[1]);
+		exit(1);
+	}
+	host = gethostbyname(argv[1]);
+	if (host == NULL) {
+		printf("Error: invalid ip address\n");
+		exit(1);
+	}
+
+	int serv = socket(AF_INET, SOCK_STREAM, 0);
+	memset(&ad, 0, sizeof(ad));
+	ad.sin_family = AF_INET;
+	ad.sin_addr = *(struct in_addr *)host->h_addr_list[0];
+	ad.sin_port = htons(8784);
+
+	if (connect(serv, (struct sockaddr *) &ad, sizeof(ad)) == 0 ) {
+		printf("Connection established\n");
+	} else 
+	{
+		printf("Connection error\n");
+		exit(1);
+	}
+
+}
